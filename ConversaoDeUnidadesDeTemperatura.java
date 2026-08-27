8da23f4a9d6c769544bca8dae2fcb4dd03b24f4a
Artur Besckow Caldasso <124211523+C4ld4ss0@users.noreply.github.com>
2026-08-27 15:14:23 -0300

    Create ConversaoDeUnidadesDeTemperatura.java

---
+++ ConversaoDeUnidadesDeTemperatura.java
@@ -0,0 +1,14 @@
+
+public class ConversaoDeUnidadesDeTemperatura{
+
+  public enum TempDesejada{
+    KELVIN, CELCIUS, FAHRENHEIT, REAUMUR, RANKINE  
+  }
+  
+  public static void main(String[] args){
+
+
+  }
+
+  
+}
