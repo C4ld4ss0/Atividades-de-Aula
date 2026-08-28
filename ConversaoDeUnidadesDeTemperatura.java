0fa5bddb9e8624578cccda64e9ba5b5e40229942
Artur Besckow Caldasso <124211523+C4ld4ss0@users.noreply.github.com>
2026-08-28 01:14:19 +0000

    COntinuação

--- ConversaoDeUnidadesDeTemperatura.java
+++ ConversaoDeUnidadesDeTemperatura.java
@@ -4,27 +4,43 @@ public class ConversaoDeUnidadesDeTemperatura{
   public enum TempDesejada{
     KELVIN, CELCIUS, FAHRENHEIT, REAUMUR, RANKINE  
   }
+  
 
   private double tempInicial;
   private double tempConversao;
 
-  private TempDesejada escalaAtual = TempDesejada.KELVIN;
+  private TempDesejada escalaInicial;
+  private TempDesejada escalaFinal;
+
   public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    ConversaoDeUnidadesDeTemperatura conversor = new ConversaoDeUnidadesDeTemperatura();
+
+    System.out.println("Informe a Escala da temperatura que vai informar:\n CELCIUS\n FAHRENHEIT\n KELVIN\n REAMUR\n RANKINE\n");
+    String entradaEscalaInicial = sc.nextLine().toUpperCase();
+    conversor.escalaInicial = TempDesejada.valueOf(entradaEscalaInicial);
+
+    System.out.println("Informe a Escala da temperatura que queres transformar: CELCIUS\n FAHRENHEIT\n KELVIN\n REAMUR\n RANKINE\n"); 
+    String entradaEscalaFinal = sc.nextLine().toUpperCase();
+    conversor.escalaFinal = TempDesejada.valueOf(entradaEscalaFinal);
 
 
+
+
+
+
+    sc.close();
   }
 
-  public void transformarParaKelvin(){
-    if(escalaAtual == TempDesejada.KELVIN){
-      tempConversao = tempInicial;
-      return;
-    } else { 
-      switch(escalaAtual){
+  public void transformarParaKelvin(){ 
+      switch(escalaInicial){
+        case KELVIN:
+          break;
         case CELCIUS:
           tempConversao = tempInicial + 273.15;
           break;
         case FAHRENHEIT:
-          tempConversao = ((tempInicial - 32.0) * 5/9) + 273.15;
+          tempConversao = ((tempInicial - 32.0) * 5.0/9.0) + 273.15;
           break;
         case REAUMUR:
           tempConversao = (tempInicial) * 5.0/4.0 + 273.15;
@@ -32,15 +48,32 @@ public class ConversaoDeUnidadesDeTemperatura{
         case RANKINE:
           tempConversao = tempInicial/1.8;
           break;
+        default:
+          System.out.println("Escala Desconhecida!!!");
       }
-      escalaAtual = TempDesejada.KELVIN;
-    }
-  public void transformarParaDesejado(){
-    if(){
-      
-    }
+    escalaInicial = TempDesejada.KELVIN;
   }
 
+  public void transformarParaDesejado(){
+      switch(escalaFinal){
+        case KELVIN:
+          break;
+        case CELCIUS:
+          tempConversao = tempConversao - 273.15;
+          break;
+        case FAHRENHEIT:
+          tempConversao = ((tempConversao - 273.15) * 9.0/5.0) + 32.0;
+          break;
+        case REAUMUR:
+          tempConversao = (tempConversao - 273.15) * 4.0/5.0;
+          break;
+        case RANKINE:
+          tempConversao = tempConversao * 1.8;
+          break;
+        default:
+          System.out.println("Escala desconhecida!!!");
+      }
+    System.out.println(" A temperatura convertida em " + escalaFinal + " é: " + tempConversao);
   }
 
   public double getTempInicial(){
@@ -50,5 +83,4 @@ public class ConversaoDeUnidadesDeTemperatura{
   public void setTempInicial(double tempInicial){
     this.tempInicial = tempInicial;
   }
-  
 }
