cfbfe883d3c748c2de2de52abe7c96e1e7393bc7
Artur Besckow Caldasso <124211523+C4ld4ss0@users.noreply.github.com>
2026-08-28 11:49:30 -0300

    Create AppTemperatura.java

---
+++ AppTemperatura.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class AppTemperatura{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe a Escala da temperatura inicial:\n CELCIUS\n FAHRENHEIT\n KELVIN\n REAUMUR\n RANKINE\n");
+        String entradaEscalaInicial = sc.nextLine().toUpperCase();
+        ConversaoDeUnidadesDeTemperatura.TempDesejada escalaInicial = ConversaoDeUnidadesDeTemperatura.TempDesejada.valueOf(entradaEscalaInicial);
+
+        System.out.println("Informe para qual Escala da temperatura que queres transformar:\n CELCIUS\n FAHRENHEIT\n KELVIN\n REAUMUR\n RANKINE\n"); 
+        String entradaEscalaFinal = sc.nextLine().toUpperCase();
+        ConversaoDeUnidadesDeTemperatura.TempDesejada escalaFinal  = ConversaoDeUnidadesDeTemperatura.TempDesejada.valueOf(entradaEscalaFinal);
+
+        System.out.println("Informe o Valor da temperatura: ");
+        double tempInicial = sc.nextDouble();
+
+        double tempEmKelvin = ConversaoDeUnidadesDeTemperatura.transformarParaKelvin(tempInicial, escalaInicial);
+        double tempFinal = ConversaoDeUnidadesDeTemperatura.transformarParaDesejado(tempEmKelvin, escalaFinal);
+        
+        System.out.println("A temperatura convertida em " + escalaFinal + " é: " + tempFinal);
+        
+        sc.close();
+   }
+}
