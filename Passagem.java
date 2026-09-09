46292563ca6faa5a971c7e5b8c784bbaedd812b1
Artur Besckow Caldasso <124211523+C4ld4ss0@users.noreply.github.com>
2026-09-09 18:18:29 -0300

    Create Passagem.java

---
+++ Passagem.java
@@ -0,0 +1,20 @@
+package Exercicio_Passagem
+
+public abstract class Passagem{
+  private String CPF;
+  private String nome;
+  private String assento;
+  private double custoPassagem;
+  
+public double custoBagagem(int qtdade, int pesos){
+
+
+}
+
+public double defineAssento(String a){
+  
+
+}
+
+
+}
